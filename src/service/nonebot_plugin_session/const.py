dda95620c4626bfcc05c24a4e0d6a8b82e1135de
XMB-HOME-WIN11\xmb.home <zhangzhenyu353@gmail.com>
2023-11-24 17:02:49 +0900

    修改memes为本机api

---
+++ src/service/nonebot_plugin_session/const.py
@@ -0,0 +1,14 @@
+from strenum import StrEnum
+
+
+class SupportedPlatform(StrEnum):
+    qq = "qq"
+    console = "console"
+    kaiheila = "kaiheila"
+    qqguild = "qqguild"
+    telegram = "telegram"
+    feishu = "feishu"
+    discord = "discord"
+    villa = "villa"
+    dodo = "dodo"
+    unknown = "unknown"
