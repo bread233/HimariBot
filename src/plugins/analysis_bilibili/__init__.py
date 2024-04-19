39137678eb24475f87ccb7874e893dbf7538cc87
bread233 <zhangzhenyu353@gmail.com>
2024-04-19 13:11:24 +0900

    bilibili update

--- src/plugins/analysis_bilibili/__init__.py
+++ src/plugins/analysis_bilibili/__init__.py
@@ -1,18 +1,30 @@
 import re
 from typing import List, Union
 from aiohttp import ClientSession
-from nonebot import on_regex, logger
+from nonebot import on_regex, logger, require
 from nonebot.adapters import Event
 from nonebot.rule import Rule
-from nonebot.adapters.onebot.v11.bot import Bot
+from nonebot.plugin import PluginMetadata
 from .analysis_bilibili import config, b23_extract, bili_keyword, search_bili_by_title
 
+require("nonebot_plugin_saa")
+from nonebot_plugin_saa import MessageFactory, MessageSegmentFactory, Text, Image  # noqa: E402
+
+__plugin_meta__ = PluginMetadata(
+    name="analysis_bilibili",
+    description="自动解析bilibili链接内容",
+    usage="https://github.com/mengshouer/nonebot_plugin_analysis_bilibili?tab=readme-ov-file#%E4%BD%BF%E7%94%A8%E6%96%B9%E5%BC%8F",
+    type="application",
+    homepage="https://github.com/mengshouer/nonebot_plugin_analysis_bilibili",
+)
+
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.69"
 }
-blacklist = getattr(config, "analysis_blacklist", [])
-group_blacklist = getattr(config, "analysis_group_blacklist", [])
-desc_blacklist = getattr(config, "analysis_desc_blacklist", [])
+
+blacklist = [str(i) for i in getattr(config, "analysis_blacklist", [])]
+group_blacklist = [str(i) for i in getattr(config, "analysis_group_blacklist", [])]
+desc_blacklist = [str(i) for i in getattr(config, "analysis_desc_blacklist", [])]
 trust_env = getattr(config, "analysis_trust_env", False)
 enable_search = getattr(config, "analysis_enable_search", False)
 
@@ -22,10 +34,10 @@ async def is_enable_search() -> bool:
 
 
 async def is_normal(event: Event) -> bool:
-    if blacklist and int(event.get_user_id()) in blacklist:
+    if blacklist and str(event.get_user_id()) in blacklist:
         return False
 
-    group_id = (
+    group_id = str(
         event.group_id
         if hasattr(event, "group_id")
         else event.channel_id
@@ -50,32 +62,61 @@ rule = Rule(is_enable_search, is_normal)
 search_bili = on_regex(r"^搜视频", rule=rule)
 
 
-async def send_msg(msg: List[Union[List[str], str]]) -> None:
-    try:
-        if not msg[0]:
-            # 解析不包含图片
-            await analysis_bili.send("\n".join(msg[1:]))
-            return
-        else:
-            try:
-                from nonebot.adapters.onebot.v11 import Message, MessageSegment
+def is_image(msg: str) -> bool:
+    return msg[-4:].lower() in [
+        ".jpg",
+        ".jpeg",
+        ".png",
+        ".gif",
+        ".bmp",
+        "jfif",
+        "webp",
+    ]
 
-                if isinstance(msg[0], list):
-                    tmp = Message([MessageSegment.image(i) for i in msg[0]])
-                else:
-                    tmp = Message(MessageSegment.image(msg[0]))
-                tmp.extend([MessageSegment.text(i) for i in msg[1:]])
-                await analysis_bili.send(tmp)
-            except ImportError:
-                # 不支持的适配器下只发送文字信息
-                await analysis_bili.send("\n".join(msg[1:]))
+
+def flatten(container):
+    for i in container:
+        if isinstance(i, (list, tuple)):
+            yield from flatten(i)
+        else:
+            yield i
+
+
+def format_msg(msg_list: List[Union[List[str], str]], is_plain_text: bool = False):
+    flatten_msg_list = list(flatten(msg_list))
+    if is_plain_text:
+        return "".join([i for i in flatten_msg_list if not is_image(i)])
+
+    msg: List[MessageSegmentFactory] = []
+
+    for i in flatten_msg_list:
+        if not i:
+            continue
+        elif is_image(i):
+            msg.append(Image(i))
+        else:
+            msg.append(Text(i))
+    return msg
+
+
+async def send_msg(msg_list: List[Union[List[str], str, bool]]) -> None:
+    if msg_list is False:
+        return
+    if msg_list is None:
+        logger.warning("此次解析的内容为空，接口可能被修改，需要更新！")
+        return
+
+    try:
+        await MessageFactory(format_msg(msg_list)).send()
+    except RuntimeError:
+        await analysis_bili.send(format_msg(msg_list, is_plain_text=True))
     except Exception as e:
         logger.exception(e)
-        logger.warning(f"{msg}\n此次解析的内容可能被风控！")
+        logger.warning(f"{msg_list}\n此次解析的内容可能被风控！")
 
 
-async def get_msg(event: Event, text: str, search: bool = False) -> List[str]:
-    group_id = (
+async def get_msg(event: Event, text: str, search: bool = False) -> Union[List[str], bool]:
+    group_id = str(
         event.group_id
         if hasattr(event, "group_id")
         else event.channel_id
@@ -102,32 +143,18 @@ async def get_msg(event: Event, text: str, search: bool = False) -> List[str]:
             if msg[-1].startswith("简介"):
                 msg[-1] = ""
 
-        return msg
+    return msg
 
 
 @analysis_bili.handle()
-async def handle_analysis(bot: Bot, event: Event) -> None:
-    uid = event.get_user_id
-    seif_uid = bot.self_id
-    uid = str(uid).split("user_id=")
-    uid = uid[1].split(",")
-    uid = uid[0]
-    if int(str(uid)) == int(str(seif_uid)):
-        return
+async def handle_analysis(event: Event) -> None:
     text = str(event.message).strip()
     msg = await get_msg(event, text)
     await send_msg(msg)
 
 
 @search_bili.handle()
-async def handle_search(bot: Bot, event: Event) -> None:
-    uid = event.get_user_id
-    seif_uid = bot.self_id
-    uid = str(uid).split("user_id=")
-    uid = uid[1].split(",")
-    uid = uid[0]
-    if int(str(uid)) == int(str(seif_uid)):
-        return
+async def handle_search(event: Event) -> None:
     text = str(event.message)[3:].strip()
     msg = await get_msg(event, text, search=True)
     await send_msg(msg)
