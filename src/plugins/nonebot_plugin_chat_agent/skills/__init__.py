ae3dfd626c55ef7e4fcb115ca389c29af073f23f
bread233 <60806911+bread233@users.noreply.github.com>
2026-05-16 00:26:41 +0900

    add external skill registry loader

---
+++ src/plugins/nonebot_plugin_chat_agent/skills/__init__.py
@@ -0,0 +1,10 @@
+from .registry import SkillDefinition, SkillRegistry, SkillResource, load_skill_file, load_skill_registry
+
+__all__ = [
+    "SkillResource",
+    "SkillDefinition",
+    "SkillRegistry",
+    "load_skill_file",
+    "load_skill_registry",
+]
+
