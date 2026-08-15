a8eb40d5335f9bd72687a0bfbb4adc221bafaaa4
bread233 <60806911+bread233@users.noreply.github.com>
2026-08-16 00:39:25 +0900

    修复插件启动错误和重复命令注册

--- src/plugins/send/send.py
+++ src/plugins/send/send.py
@@ -1,11 +1,11 @@
 import time
 from nonebot import on_command
-from nonebot.permission import SUPERUSER
 from nonebot.adapters.onebot.v11 import Bot, Message, GroupMessageEvent
 from nonebot.params import CommandArg
-send = on_command('.send', priority=5)
-notice = on_command('.notice', priority=5, permission=SUPERUSER)
 from .utils import sendtosuperuser
+
+send = on_command('.send', priority=5)
+
 
 @send.handle()
 async def send_receive(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
@@ -18,4 +18,4 @@ async def send_receive(bot: Bot, event: GroupMessageEvent, args: Message = Comma
     timenow = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
     sendmsg = timenow+'\n群聊：'+group_name+'\n发送者：'+name+'\n'+args_in
     await sendtosuperuser(sendmsg)
-    await send.finish(f'已经把意见传达给Master了喵！')
+    await send.finish('已经把意见传达给Master了喵！')
